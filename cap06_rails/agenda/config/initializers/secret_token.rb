ef4767406eea1d7e0c6c657b55f6c773d3e2ef0d
Renata Nobre <renata.vnobre@gmail.com>
2014-04-30 17:25:25 -0300

    Exemplo da agenda de eventos do cap 06

---
+++ cap06_rails/agenda/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agenda::Application.config.secret_key_base = '1cad2020b5de3b066c56d7bf1ff4b260429ee1096b094bfe56f3fbe9bc9c8007c497b3a4afcaa9313da6931940f242c02ba9804a7282e38f32c6b9138169cd64'
