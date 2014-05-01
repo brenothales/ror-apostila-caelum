557679a4bfdb479b2f932ef66158fd71d7db2e63
Renata Nobre <renata.vnobre@gmail.com>
2014-05-01 17:18:50 -0300

    Exemplos do cap 08
    Routes

--- cap07/vota_prato/config/routes.rb
+++ cap07/vota_prato/config/routes.rb
@@ -1,4 +1,7 @@
 VotaPrato::Application.routes.draw do
+  match 'ola' => 'ola_mundo#index', via: 'get'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +56,3 @@ VotaPrato::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
